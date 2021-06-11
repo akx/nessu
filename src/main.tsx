abfd418d470c3e27225a793979523cb3746811f6
Aarni Koskela <akx@iki.fi>
2021-06-11 15:19:06 +0300

    Add buildy bits

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import init from "./nes-rust";
-// @ts-ignore
-await init();
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+async function boot() {
+  // @ts-ignore
+  await init();
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+boot();
